7c544e6c99f6e0ee885daa1dd230646b530277f5
sherif shalaby <s.shalaby@chrometic.com>
2018-03-12 14:07:02 +0200

    used main router

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 var db = require('./mongooseSchema')
-const express = require('express'), app = express(),  bodyParser = require('body-parser'), slackListener = require('./slackListener.js')
-
+const express = require('express'),
+ app = express(),  
+ bodyParser = require('body-parser'), slackListener = require('./slackListener.js')
+ , mainRouter = require('./mainRouter.js')
 app.use(bodyParser.json())
  
 app.use(slackListener)
+app.use(mainRouter)
 module.exports = app

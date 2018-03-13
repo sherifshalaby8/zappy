6df5eab76c0dd75284b88f24b66e4b34b7d4ead8
sherif shalaby <s.shalaby@chrometic.com>
2018-03-13 15:18:28 +0200

    allow cors

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+var cors = require('cors')
 const express = require('express'),
  app = express(),  
  bodyParser = require('body-parser'), slackListener = require('./slackListener.js')
  , tweetsApi = require('./api/tweets')
+app.use(cors());
 app.use(bodyParser.json())
 app.use(slackListener)
 app.use(tweetsApi)
